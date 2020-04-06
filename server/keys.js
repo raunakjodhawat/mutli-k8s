1152c84e212fcdbe2dde355218149d7e9b39f3c9
Raunak Jodhawat <raunakjodhawat@gmail.com>
2020-04-06 09:17:21 -0400

    Initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgPassword: process.env.PGPASS,
+    pgPort: process.env.PGPORT,
+    pgDatabase: process.env.PGDATABASE
+}
